10416957f63b9ddf67c21229c3da20e5a6ec5c39
Danilo S Andrade <danilo.s.andrade@gmail.com>
2026-07-30 17:38:23 -0300

    fix(wezterm): enable kitty keyboard protocol for Ctrl+,/. in helix
    Without it wezterm can't send Ctrl+comma/Ctrl+period as distinct
    key events, so the helix goto_previous/next_buffer bindings never fired.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -16,6 +16,7 @@ config.colors = {
   split = "#47FF9C",
 }
 
+config.enable_kitty_keyboard = true
 config.font_size = 19
 config.window_decorations = "RESIZE"
 config.send_composed_key_when_left_alt_is_pressed = false
